d39afccf29f4d91a7c1752ef0d619c4c7a277986
Chris Abdo <christophermabdo@icloud.com>
2021-01-20 15:11:36 +0000

    refractor html and interface

--- views/interface.js
+++ views/interface.js
@@ -1,1 +1,20 @@
 // document.getElementById("textbox").innerHTML = txt
+let entries = []
+
+let button = document.getElementById('create-button')
+button.addEventListener("click", function() {
+    let area = document.getElementById('textbox')
+    let text = area.value
+    let note = new Note(text)
+    area.value = ""
+    entries.push(note)
+    viewEntries()
+    })
+    
+    function viewEntries() {
+        let entriesDiv = document.getElementById("notes")
+        notes.innerHTML = ""
+        entries.forEach(function(note) {
+            entriesDiv.appendChild(note.elementForDisplay())
+        });
+    } 
