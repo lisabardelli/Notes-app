0b0bf03bc3e9a1c16d6f81e0d467d83a4a781945
calavell <calavell@tcd.ie>
2021-01-20 16:17:03 +0000

    adding hash routes unfinished

--- src/note.js
+++ src/note.js
@@ -1,11 +1,20 @@
 class Note {
   constructor(text) {
-    this.text = text
+    this.text = text;
+    this.id;
   }
 
   elementForDisplay() {
-    let textElement = document.createElement("p")
+    let textElement = document.createElement("a")
+    textElement.setAttribute("id", `${this.id}`)
+    textElement.setAttribute("href", `#${this.id}`)
     textElement.textContent = this.text.substring(0, 20)
     return textElement
   }
+
+  assignId(number) {
+    this.id = number;
+  }
 }
+
+// myPara.setAttribute("id", "id_you_like");
--- views/interface.js
+++ views/interface.js
@@ -8,13 +8,30 @@ button.addEventListener("click", function() {
     let note = new Note(text)
     area.value = ""
     entries.push(note)
+    note.assignId(entries.indexOf(note))
     viewEntries()
     })
-    
+
     function viewEntries() {
         let entriesDiv = document.getElementById("notes")
         notes.innerHTML = ""
         entries.forEach(function(note) {
             entriesDiv.appendChild(note.elementForDisplay())
         });
-    } 
+    }
+
+
+
+    // makeUrlChangeShowNoteForCurrentPage();
+    //
+    // function makeUrlChangeShowNoteForCurrentPage() {
+    //   window.addEventListener("hashchange", showNoteForCurrentPage);
+    // };
+    //
+    // function showNoteForCurrentPage() {
+    //
+    // }
+    //
+    // function showNoteById(notes, id) {
+    //   return notes[id];
+    // }
